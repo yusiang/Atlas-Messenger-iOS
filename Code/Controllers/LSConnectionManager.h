eba8355078d0e1cb4c1f811524092a542cfb7e81
Blake Watters <blake@layer.com>
2014-06-27 09:26:06 -0700

    Sketch headers

--- Code/Controllers/LSConnectionManager.h
+++ Code/Controllers/LSConnectionManager.h
@@ -8,7 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
-@interface LSConnectionManager : NSObject <NSURLSessionDelegate>
+// TODO: Rename to `LSAuthenticationManager`
+@interface LSConnectionManager : NSObject <NSURLSessionDelegate> // TODO: Move `NSURLSessionDelegate` to the class extension
+
+// TODO: initWithBaseURL:
+
+@property (nonatomic, readonly) NSString *authToken;
+@property (nonatomic, readonly) NSString *email;
+
+- (void)signUpWithEmail:(NSString *)email password:(NSString *)password completion:(void(^)(BOOL success, NSError *error))completion;
+// TODO: Add a login method
+// TODO: Add some way to manage resuming an existing session
 
 - (void)requestLayerIdentityTokenWithNonce:(NSString *)nonce completion:(void(^)(NSString *identityToken, NSError *error))completion;
 
