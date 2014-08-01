e6825a839a43a47d1526c17325192ac622555dde
Zac White <zac@velosmobile.com>
2014-08-01 12:11:47 -0700

    Added a simple version view to the top of the conversation list table view.

---
+++ Code/Views/LSVersionView.h
@@ -0,0 +1,16 @@
+//
+//  LSVersionView.h
+//  LayerSample
+//
+//  Created by Zac White on 7/31/14.
+//  Copyright (c) 2014 Layer, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSVersionView : UIView
+
+@property (readonly, nonatomic) UILabel *topLabel;
+@property (readonly, nonatomic) UILabel *bottomLabel;
+
+@end
---
+++ Code/Views/LSVersionView.m
@@ -0,0 +1,77 @@
+//
+//  LSVersionView.m
+//  LayerSample
+//
+//  Created by Zac White on 7/31/14.
+//  Copyright (c) 2014 Layer, Inc. All rights reserved.
+//
+
+#import "LSVersionView.h"
+#import "LSUIConstants.h"
+
+@interface LSVersionView ()
+
+@property (strong, nonatomic) UILabel *topLabel;
+@property (strong, nonatomic) UILabel *bottomLabel;
+
+@end
+
+@implementation LSVersionView
+
+- (void)LSVersionView_setup
+{
+    self.backgroundColor = [UIColor clearColor];
+
+    self.topLabel = [[UILabel alloc] init];
+    self.topLabel.font = LSBoldFont(12.0);
+    self.topLabel.textColor = LSGrayColor();
+    self.topLabel.textAlignment = NSTextAlignmentCenter;
+
+    self.bottomLabel = [[UILabel alloc] init];
+    self.bottomLabel.font = LSBoldFont(11.0);
+    self.bottomLabel.textColor = LSGrayColor();
+    self.bottomLabel.textAlignment = NSTextAlignmentCenter;
+
+    [self addSubview:self.topLabel];
+    [self addSubview:self.bottomLabel];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    CGRect topLabelFrame, bottomLabelFrame;
+    CGRectDivide(self.bounds, &topLabelFrame, &bottomLabelFrame, (int)(self.bounds.size.height / 2.0), CGRectMinYEdge);
+    self.topLabel.frame = topLabelFrame;
+    self.bottomLabel.frame = bottomLabelFrame;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    if (!(self = [super initWithFrame:frame])) return nil;
+
+    [self LSVersionView_setup];
+
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if (!(self = [super initWithCoder:aDecoder])) return nil;
+
+    [self LSVersionView_setup];
+
+    return self;
+}
+
+static const CGFloat LSVersionViewXPadding = 5.0f;
+static const CGFloat LSVersionViewYPadding = 5.0f;
+
+- (CGSize)sizeThatFits:(CGSize)size
+{
+    CGSize topSize = [self.topLabel sizeThatFits:size];
+    CGSize bottomSize = [self.bottomLabel sizeThatFits:size];
+
+    return CGSizeMake(MAX(topSize.width, bottomSize.width) + 2 * LSVersionViewXPadding, topSize.height + bottomSize.height + 2 * LSVersionViewYPadding);
+}
+
+@end
