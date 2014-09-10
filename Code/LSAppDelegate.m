2517282671daedcfc2a1a9ea6a26cdd67f3e3c6f
Drew Moxon <drew.moxon@gmail.com>
2014-09-10 16:54:43 -0700

    Changing to production

--- Code/LSAppDelegate.m
+++ Code/LSAppDelegate.m
@@ -38,7 +38,7 @@ extern void LYRSetLogLevelFromEnvironment();
     LYRSetLogLevelFromEnvironment();
     
     // Setup environment configuration
-    LSEnvironment environment = LSDevelopmentEnvironment;
+    LSEnvironment environment = LSProductionEnvironment;
     
     // Kicking off Crashlytics
     [Crashlytics startWithAPIKey:@"0a0f48084316c34c98d99db32b6d9f9a93416892"];
