ceb28a624d9c24b9f21ab252cb65f5b1d0436737
Blake Watters <blake@layer.com>
2014-12-23 11:39:47 -0800

    Remove conflict markers

--- Code/LSAppDelegate.m
+++ Code/LSAppDelegate.m
@@ -468,10 +468,6 @@ void LYRTestResetConfiguration(void)
     
     [[UIBarButtonItem appearanceWhenContainedIn:[UINavigationBar class], nil] setTitleTextAttributes:@{NSFontAttributeName : LYRUIMediumFont(16)} forState:UIControlStateNormal];
     [[UIBarButtonItem appearanceWhenContainedIn:[UINavigationBar class], nil] setTintColor:LYRUIBlueColor()];
-<<<<<<< HEAD
-
-=======
->>>>>>> d9d7cb34f3fd7430fd27d17b7ab3168065f0f114
 }
 
 #pragma mark - Screen Shot Handler
