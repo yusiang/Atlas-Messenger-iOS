04de846a68832edb2ea2e6472e1bf0643717c07b
Kevin Coleman <kcoleman731@gmail.com>
2015-01-07 10:18:41 -0800

    Changing screenshot handler method sig

--- Code/LSAppDelegate.m
+++ Code/LSAppDelegate.m
@@ -205,7 +205,7 @@ void LYRUITestResetConfiguration(void)
                                                object:nil];
     
     [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(screenshotTaken:)
+                                             selector:@selector(screenshotNotification:)
                                                  name:UIApplicationUserDidTakeScreenshotNotification
                                                object:nil];
 }
@@ -479,7 +479,7 @@ void LYRUITestResetConfiguration(void)
 
 #pragma mark - Screen Shot Handler Methods
 
-- (void)screenshotTaken:(NSNotification *)notification
+- (void)screenshotNotification:(NSNotification *)notification
 {
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Report Issue?"
                                                         message:@"Would you like to report a bug with the sample app?"
