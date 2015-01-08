e7190ae49713e3eb0491b1516da3e767bc0e1347
Ben Blakley <brblakley@gmail.com>
2015-01-08 14:01:29 -0800

    Fixed typos in persistence manager header documentation.

--- Code/Controllers/LSPersistenceManager.h
+++ Code/Controllers/LSPersistenceManager.h
@@ -11,8 +11,8 @@
 #import "LSUser.h"
 
 /**
- @abstract The `LSPersistenceManager` provides an interface for persiting and querying, session and contact
- data related to the Layer Sample Application
+ @abstract The `LSPersistenceManager` provides an interface for persisting and querying, session and user
+ data related to the Layer sample application.
  */
 @interface LSPersistenceManager : NSObject
 
@@ -27,10 +27,9 @@
 
 /**
  @abstract Designated initializer when running the application.
- @param The path to which data should be persisted.
+ @param The path where the data should be persisted.
  */
 + (instancetype)persistenceManagerWithStoreAtPath:(NSString *)path;
-
 
 ///---------------------------------------
 /// @name Persisting
@@ -38,21 +37,19 @@
 
 /**
  @abstract Persists an `NSSet` of `LSUser` objects to the specified path.
- @param users The `NSSet` of `LSUeser objects to be persisted.
+ @param users The `NSSet` of `LSUser` objects to be persisted.
  @param error A reference to an `NSError` object that will contain error information in case the action was not successful.
- @return A Boolen value indicating if the operation was succesful
+ @return A boolean value indicating if the operation was successful.
  */
 - (BOOL)persistUsers:(NSSet *)users error:(NSError **)error;
 
 /**
- @abstract Persists an `LSSession` object for the currently authenticated User.
+ @abstract Persists an `LSSession` object for the currently authenticated user.
  @param session The `LSSession` object to be persisted.
  @param error A reference to an `NSError` object that will contain error information in case the action was not successful.
- @return A Boolen value indicating if the operation was succesful
+ @return A boolean value indicating if the operation was successful.
  */
-
 - (BOOL)persistSession:(LSSession *)session error:(NSError **)error;
-
 
 ///---------------------------------------
 /// @name Fetching
@@ -71,28 +68,27 @@
 - (LSSession *)persistedSessionWithError:(NSError **)error;
 
 /**
- @abstract Returns an `NSSet` of `LSUser` objects who's `userID` properties match those supplied in an `NSSet` of identifiers.
+ @abstract Returns an `NSSet` of `LSUser` objects whose `userID` properties match those supplied in an `NSSet` of identifiers.
  @param identifiers An `NSSet` of `NSString` objects representing user identifiers.
- @return An `NSSet` of `LSUser` objects
+ @return An `NSSet` of `LSUser` objects.
  */
 - (NSSet *)participantsForIdentifiers:(NSSet *)identifiers;
 
 /**
- @abstract Performs a search across the `fullName` property on all persisted `LSUser` objects for the supplied string
+ @abstract Performs a search across the `fullName` property on all persisted `LSUser` objects for the supplied string.
  @param searchString The string object for which the search is performed.
- @param completion   The completion block called when search completes.
+ @param completion The completion block called when search completes.
  */
-- (void)performParticipantSearchWithString:(NSString *)searchString completion:(void(^)(NSArray *contacts, NSError *error))completion;
-
+- (void)performParticipantSearchWithString:(NSString *)searchString completion:(void(^)(NSArray *participants, NSError *error))completion;
 
 ///---------------------------------------
 /// @name Deletion
 ///---------------------------------------
 
 /**
- @abstract Deletes all objects currently persisted in teh persistence manager.
+ @abstract Deletes all objects currently persisted in the persistence manager.
  @param error A reference to an `NSError` object that will contain error information in case the action was not successful.
- @return A Boolen value indicating if the operation was succesful.
+ @return A boolean value indicating if the operation was successful.
  */
 - (BOOL)deleteAllObjects:(NSError **)error;
 
