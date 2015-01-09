c4abdafd41b6299c5a4bc32d75a0b881dd869ee7
Ben Blakley <brblakley@gmail.com>
2015-01-08 17:41:40 -0800

    Fixed typos in API manager header documentation.

--- Code/Controllers/LSAPIManager.h
+++ Code/Controllers/LSAPIManager.h
@@ -32,7 +32,7 @@ extern NSString *const LSUserDidDeauthenticateNotification;
 @property (nonatomic, readonly) LSSession *authenticatedSession;
 
 /**
- @abstract The current authenticated URL Session configuration or `nil` if not yet authenticated.
+ @abstract The current authenticated URL session configuration or `nil` if not yet authenticated.
  */
 @property (nonatomic, readonly) NSURLSessionConfiguration *authenticatedURLSessionConfiguration;
 
@@ -42,32 +42,32 @@ extern NSString *const LSUserDidDeauthenticateNotification;
 ///------------------------------------
 
 /**
- @abstract Registers a new user with the Layer sample backend rails applicaiton
- @param user The model object representing the user attempting to authenticate
- @param completion The completion block that will be called upon completion of the registration operation. Completion block cannot be `nil`
+ @abstract Registers a new user with the Layer sample backend Rails application.
+ @param user The model object representing the user attempting to authenticate.
+ @param completion The completion block that will be called upon completion of the registration operation. Completion block cannot be `nil`.
  */
 - (void)registerUser:(LSUser *)user completion:(void(^)(LSUser *user, NSError *error))completion;
 
 /**
- @abstract Autheticates an existing users with the Layer sample backend rails applicaiton. This method takes a nonce value that must be 
- obtained from LayerKit. It returns an identity token in the completion block that can be used to authenticate LayerKit
- @param email The email address for the user attempting to authenticate
- @param password The password for the user attempting to authenticate
- @param nonce The nonce obtained from LayerKit
+ @abstract Authenticates an existing user with the Layer sample backend Rails application. This method takes a nonce value that must be
+ obtained from LayerKit. It returns an identity token in the completion block that can be used to authenticate LayerKit.
+ @param email The email address for the user attempting to authenticate.
+ @param password The password for the user attempting to authenticate.
+ @param nonce The nonce obtained from LayerKit.
  @param completion The completion block that is called upon completion of the authentication operation. Upon succesful authentication, 
- an identityToken will be returned. Completion block cannot be `nil`
+ an identityToken will be returned. Completion block cannot be `nil`.
  */
 - (void)authenticateWithEmail:(NSString *)email password:(NSString *)password nonce:(NSString *)nonce completion:(void(^)(NSString *identityToken, NSError *error))completion;
 
 /**
- @abstract Loads all contacts from the Layer sample backend rails application
- @param completion The completion block that is called upon succesfull load of contacts. Completion block cannot be `nil`
+ @abstract Loads all contacts from the Layer sample backend Rails application.
+ @param completion The completion block that is called upon successfully loading contacts. Completion block cannot be `nil`.
  */
 - (void)loadContactsWithCompletion:(void(^)(NSSet *contacts, NSError *error))completion;
 
 /**
- @abstract Deletes all contacts from the Layer sample backend rails application
- @param completion The completion block that is call upon succesfull deletion of all contacts. Completion block cannot be `nil`
+ @abstract Deletes all contacts from the Layer sample backend Rails application.
+ @param completion The completion block that is call upon successful deletion of all contacts. Completion block cannot be `nil`.
  */
 - (void)deleteAllContactsWithCompletion:(void(^)(BOOL completion, NSError *error))completion;
 
@@ -76,15 +76,15 @@ extern NSString *const LSUserDidDeauthenticateNotification;
 ///------------------------------
 
 /**
- @abstract Resumes a Layer sample app session
- @param session The model object for the current session
- @param error   A reference to an `NSError` object that will contain error information in case the action was not successful.
- @return A Boolean value that indicates if the application has a valid session
+ @abstract Resumes a Layer sample app session.
+ @param session The model object for the current session.
+ @param error A reference to an `NSError` object that will contain error information in case the action was not successful.
+ @return A boolean value that indicates if the manager has a valid session.
  */
 - (BOOL)resumeSession:(LSSession *)session error:(NSError **)error;
 
 /**
- @abstract Deauthenticates the Layer Sample app by discarding it's `LSSession` object.
+ @abstract Deauthenticates the Layer sample app by discarding its `LSSession` object.
  */
 - (void)deauthenticate;
 
