d0838b5f3622e62a0f5737da49fcd8f1229549c4
Ben Blakley <brblakley@gmail.com>
2015-01-12 10:31:52 -0800

    Changed utility functions white space for consistency.

--- Code/Controllers/LSUtilities.m
+++ Code/Controllers/LSUtilities.m
@@ -24,16 +24,22 @@ NSString *LSLayerConfigurationURL(LSEnvironment environment)
     switch (environment) {
         case LYRUIProduction:
             return @"https://conf.lyr8.net/conf";
+
         case LYRUIDevelopment:
             return @"https://conf.lyr8.net/conf";
+
         case LYRUIStage1:
              return @"https://conf.stage1.lyr8.net/conf";
+
         case LYRUIDev1:
             return @"https://dev-1.preview.layer.com:444/conf";
+
         case LSTestEnvironment:
             return @"https://conf.lyr8.net/conf";
+
         case LSAdHoc:
             return @"https://130.211.117.22:444/conf";
+
         default:
             return nil;
     }
@@ -44,16 +50,22 @@ NSUUID *LSLayerAppID(LSEnvironment environment)
     switch (environment) {
         case LYRUIProduction:
             return [[NSUUID alloc] initWithUUIDString:@"9ec30af8-5591-11e4-af9e-f7a201004a3b"];
+
         case LYRUIDevelopment:
             return [[NSUUID alloc] initWithUUIDString:@"361ff3ca-70e0-11e4-a4ef-1dec000000e6"];
+
         case LYRUIStage1:
             return [[NSUUID alloc] initWithUUIDString:@"24f43c32-4d95-11e4-b3a2-0fd00000020d"];
+
         case LYRUIDev1:
             return [[NSUUID alloc] initWithUUIDString:@"9ae66b44-1682-11e4-92e4-0b53000001d0"];
+
         case LSTestEnvironment:
             return [[NSUUID alloc] initWithUUIDString:@"46dfa7da-6d1d-11e4-a787-e6f4000000e7"];
+
         case LSAdHoc:
             return [[NSUUID alloc] initWithUUIDString:@"d354316c-63be-11e4-841a-364a00000bce"];
+
         default:
             return nil;
     }
@@ -67,7 +79,7 @@ NSString *LSApplicationDataDirectory(void)
 
 LSPersistenceManager *LSPersitenceManager(void)
 {
-    if (LSIsRunningTests()){
+    if (LSIsRunningTests()) {
         return [LSPersistenceManager persistenceManagerWithInMemoryStore];
     }
     return [LSPersistenceManager persistenceManagerWithStoreAtPath:[LSApplicationDataDirectory() stringByAppendingPathComponent:@"PersistentObjects"]];
@@ -78,6 +90,7 @@ void LSAlertWithError(NSError *error)
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Unexpected Error"
                                                         message:error.localizedDescription
                                                        delegate:nil
-                                              cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
     [alertView show];
 }
