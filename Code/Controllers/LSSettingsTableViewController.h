b4af769dced99b5dff108f80e8f5ebbb8214b91e
Ben Blakley <brblakley@gmail.com>
2015-01-13 09:52:40 -0800

    Fixed typos in settings view controller header documentation.

--- Code/Controllers/LSSettingsTableViewController.h
+++ Code/Controllers/LSSettingsTableViewController.h
@@ -13,22 +13,22 @@
 @class LSSettingsTableViewController;
 
 /**
- @abstract The `LSSettingsTableViewControllerDelegate` protocol informs the reciever to events that have occured 
+ @abstract The `LSSettingsTableViewControllerDelegate` protocol informs the receiver of events that have occurred
  within the controller.
  */
 @protocol LSSettingsTableViewControllerDelegate <NSObject>
 
 /**
- @abstract Informs the reciever that a Logout button has been tapped in the controller. 
- @param settingsTableViewController The controller in which the selection occured.
+ @abstract Informs the receiver that a logout button has been tapped in the controller.
+ @param settingsTableViewController The controller in which the selection occurred.
  */
 - (void)logoutTappedInSettingsTableViewController:(LSSettingsTableViewController *)settingsTableViewController;
 
 @end
 
 /**
- @abstract The `LSSettingsTableViewController` Presents a user interface for viewing and configuring application setting
- in adition to information related to the application.
+ @abstract The `LSSettingsTableViewController` presents a user interface for viewing and configuring application settings
+ in addition to information related to the application.
  */
 @interface LSSettingsTableViewController : UITableViewController
 
