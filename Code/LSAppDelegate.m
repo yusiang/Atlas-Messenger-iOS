41a189089682cf085b26108ac5e6758a58a94c3d
Kevin Coleman <kcoleman731@gmail.com>
2015-01-21 13:05:50 -0800

    Switching env to production on branch

--- Code/LSAppDelegate.m
+++ Code/LSAppDelegate.m
@@ -54,7 +54,7 @@ void LSTestResetConfiguration(void)
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Set up environment configuration
-    [self configureApplication:application forEnvironment:LSTestEnvironment];
+    [self configureApplication:application forEnvironment:LSProductionEnvironment];
     [self initializeCrashlytics];
     [self initializeHockeyApp];
     [self removeSplashView];
