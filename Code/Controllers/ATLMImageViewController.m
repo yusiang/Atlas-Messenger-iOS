4a791a37955dcf5dcb8f3a443dfbc2ec875e8d65
Klemen Verdnik <klemen.verdnik@gmail.com>
2015-02-18 14:53:40 -0800

    Potentially fixes APPS-1180 - Dispatching progress view UI changes asynchronously onto main thread.

--- Code/Controllers/ATLMImageViewController.m
+++ Code/Controllers/ATLMImageViewController.m
@@ -314,7 +314,7 @@ static CGFloat const ATLMImageViewControllerProgressViewSize = 128.0f;
 {
     // Queue UI updates onto the main thread, since LYRProgress performs
     // delegate callbacks from a background thread.
-    dispatch_sync(dispatch_get_main_queue(), ^{
+    dispatch_async(dispatch_get_main_queue(), ^{
         BOOL progressCompleted = progress.fractionCompleted == 1.0f;
         [self.progressView setProgress:progress.fractionCompleted animated:YES];
         // After transfer completes, remove self for delegation.
